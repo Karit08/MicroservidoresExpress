1ac5bdf163d61e0baab820e5e21d6e79719a2030
Karit08 <karla.jm.ingmec49@gmail.com>
2023-11-10 15:01:02 -0600

    Se realizaron comentarios en el archivo index de BD

--- BD/index.js
+++ BD/index.js
@@ -1,10 +1,9 @@
 const server = require("./src/server");
 // const mongo = require("./src/config/connect");
 
-const {Character, Film } = require("./src/database");
+// const {Character, Film, Planet} = require("./src/database");
 
 // Character.list().then(res=> console.log(res[0]));
-
 
 server.listen(3004, ()=>{
     console.log("DB service listening on port 3004");
