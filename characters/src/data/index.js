928fbd7e9e207490ad48dbe05f8208df60fb658e
Karit08 <karla.jm.ingmec49@gmail.com>
2023-11-10 15:28:53 -0600

    Se modifico el archivo index, se cambio la dirección del axios

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -1,19 +1,22 @@
-const character = require("./characters.json");
+// const character = require("./characters.json");
 const axios = require("axios");
 
 module.exports={
     // list: async () => { return character; },
     list: async () =>{
-        const { data } = await axios.get('http://localhost:3004/Character');
+        const { data } = await axios.get('http://basedatos:3004/Character'); //basedatos es el contenedor de Docker
+        // const { data } = await axios.get('http://localhost:3004/Character');
         // return await fetch('http://localhost:3004/Character').then(res => res.json());
         return data; 
     },
     get: async(id) =>{
-        const { data } = await axios.get(`http://localhost:3004/Character/${id}`);
+        const { data } = await axios.get(`http://basedatos:3004/Character/${id}`);
+        // const { data } = await axios.get(`http://localhost:3004/Character/${id}`);
         return data; 
     },
     insert: async (body) =>{
-        const {data} = await axios.post('http://localhost:3004/Character', body);
+        const {data} = await axios.post('http://basedatos:3004/Character', body);
+        // const {data} = await axios.post('http://localhost:3004/Character', body);
         return data; 
     }
 }
